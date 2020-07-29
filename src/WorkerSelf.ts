38c487cdb867b356a224dc54f0f7441dc2996131
Dario Segura <darionco.dev@gmail.com>
2020-07-29 13:10:49 -0400

    [SELF] Makes use of the node utility functions

--- src/WorkerSelf.ts
+++ src/WorkerSelf.ts
@@ -1,21 +1,8 @@
 import {MessagePort} from 'worker_threads';
-import {isNodeJS} from './isNodeJS';
+import {getModule, isNodeJS} from './envNodeJS';
 
 // fix `self` type for compilation
 declare const self: WorkerGlobalScope;
-
-// declare `__non_webpack_require__` for WebPack environments
-// eslint-disable-next-line camelcase
-declare const __non_webpack_require__: any;
-
-function checkDynamicImport(): boolean {
-    try {
-        import(`${null}`).catch(() => false);
-        return true;
-    } catch {
-        return false;
-    }
-}
 
 class WorkerSelfWrapper {
     private _ready: Promise<void> = this.initialize();
@@ -30,23 +17,8 @@ class WorkerSelfWrapper {
 
     private async initialize(): Promise<void> {
         if (isNodeJS()) {
-            try {
-                const dynamicImportsAvailable = checkDynamicImport();
-
-                let WorkerThreads;
-
-                if (dynamicImportsAvailable) {
-                    const libName = 'worker_threads';
-                    WorkerThreads = await import(libName);
-                } else {
-                    WorkerThreads =
-                        typeof module !== 'undefined' && typeof module.require === 'function' && module.require('worker_threads') ||
-                        // eslint-disable-next-line camelcase
-                        typeof __non_webpack_require__ === 'function' && __non_webpack_require__('worker_threads') ||
-                        typeof require === 'function' && require('worker_threads'); // eslint-disable-line
-                }
-                this._self = WorkerThreads.parentPort;
-            } catch (e) {} // eslint-disable-line
+            const WorkerThreads = await getModule('worker_threads');
+            this._self = WorkerThreads.parentPort;
         } else {
             this._self = self;
         }
