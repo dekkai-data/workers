4ecd51b3ab4a997ba198893aef0b40b2b2a91e2b
Dario Segura <darionco.dev@gmail.com>
2020-07-29 13:12:26 -0400

    [BUILD] Cleaned up rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -3,37 +3,10 @@ const path = require('path');
 const typescript = require('rollup-plugin-typescript2');
 const sourceMaps = require('rollup-plugin-sourcemaps');
 const globby = require('globby');
-const server = require('live-server');
 
 const buildDir = path.resolve(__dirname, 'dist');
 
-const extensions = [
-    '.js', '.jsx', '.ts', '.tsx',
-];
-
-function liveServer(options = {}) {
-    const defaultParams = {
-        file: 'index.html',
-        host: '0.0.0.0',
-        logLevel: 2,
-        open: false,
-        port: 8080,
-        root: '.',
-        wait: 200,
-    };
-
-    const params = Object.assign({}, defaultParams, options);
-
-    server.start(params);
-    return {
-        name: 'liveServer',
-        generateBundle() {
-            console.log(`live-server running on ${params.port}`);
-        }
-    };
-}
-
-function generateClientConfig(startDevServer = false) {
+function generateClientConfig() {
     const input = {};
     globby.sync([
         path.join('src/', '/**/*.ts'),
@@ -44,7 +17,7 @@ function generateClientConfig(startDevServer = false) {
         input[path.join(parsed.dir.substr('src/'.length), parsed.name)] = file;
     });
 
-    const config = {
+    return {
         input: input,
         treeshake: true,
         output: {
@@ -66,30 +39,11 @@ function generateClientConfig(startDevServer = false) {
             'worker_threads',
         ],
     };
-
-    if (startDevServer) {
-        config.plugins.push(sourceMaps());
-        config.plugins.push(liveServer({
-            port: 8090,
-            host: '0.0.0.0',
-            root: path.resolve(__dirname, 'src/static'),
-            file: 'index.html',
-            open: false,
-            wait: 500,
-            // proxy: [['/api', 'http://127.0.0.1:8080']], // not needed for now
-            watch: [path.resolve(__dirname, 'dist/scripts')],
-            mount: [
-                ['/scripts', path.resolve(__dirname, 'dist/scripts')],
-            ],
-        }));
-    }
-
-    return config;
 }
 
-module.exports = function generator(args) {
+module.exports = function generator() {
     const config = [];
-    config.push(generateClientConfig(args['config-dev-server']));
+    config.push(generateClientConfig());
 
     return config;
 };
