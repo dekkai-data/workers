801b57c36ef2649dfc9b533f461eadb9d4fc4632
Dario Segura <darionco.dev@gmail.com>
2020-07-29 13:09:03 -0400

    [NODE] Renamed module and added utility functions

---
+++ src/envNodeJS.ts
@@ -0,0 +1,34 @@
+const kIsNodeJS: boolean = Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]';
+export function isNodeJS(): boolean {
+    return kIsNodeJS;
+}
+
+function checkDynamicImport(): boolean {
+    try {
+        import(`${null}`).catch(() => false);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+const kSupportsDynamicImport: boolean = checkDynamicImport();
+export function supportsDynamicImport(): boolean {
+    return kSupportsDynamicImport;
+}
+
+// declare `__non_webpack_require__` for WebPack environments
+// eslint-disable-next-line camelcase
+declare const __non_webpack_require__: any;
+
+export async function getModule(mod: string): Promise<any> {
+    if (kSupportsDynamicImport) {
+        return await import(mod);
+    } else {
+        return typeof module !== 'undefined' && typeof module.require === 'function' && module.require(mod) ||
+                // eslint-disable-next-line camelcase
+                typeof __non_webpack_require__ === 'function' && __non_webpack_require__(mod) ||
+                typeof require === 'function' && require(mod); // eslint-disable-line
+    }
+    return null;
+}
--- src/isNodeJS.ts
+++
@@ -1,5 +0,0 @@
-const kIsNodeJS: boolean = Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]';
-
-export function isNodeJS(): boolean {
-    return kIsNodeJS;
-}
